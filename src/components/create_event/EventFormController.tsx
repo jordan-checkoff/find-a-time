88e1732277aeb3010d23cf0837dc0c800d0fe703
Jordan Checkoff <jordan.checkoff@gmail.com>
2024-01-04 21:54:03 -0800

    timezone for event creation

--- src/components/create_event/EventFormController.tsx
+++ src/components/create_event/EventFormController.tsx
@@ -4,6 +4,11 @@ import EventForm from "./EventForm"
 import { useNavigate } from 'react-router-dom';
 import { createEvent } from "../../utils/api_calls";
 import dayjs from "dayjs";
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
 
 
 export default function EventFormController() {
@@ -27,18 +32,20 @@ export default function EventFormController() {
         const endtime = data.endtime
     
         if (starttime && endtime) {
-            const start_times = data.dates.map(x => x.hour(starttime.hour()).startOf('hour').valueOf())
+            const start_times = data.dates.map(x => x.hour(starttime.hour()).startOf('hour').tz(data.timezone, true).valueOf())
             let num_blocks = (endtime.hour() - starttime.hour()) * 2
             if (num_blocks < 0) {
                 num_blocks += 48
             }
+
+          console.log(start_times, num_blocks, data.timezone)
     
-          const res = await createEvent({title: data.title, start_times: start_times, num_blocks: num_blocks})
-          if (res.statusCode != 200) {
-            console.log('error')
-          } else {
-            navigate("event/" + res.id)
-          }
+          // const res = await createEvent({title: data.title, start_times: start_times, num_blocks: num_blocks})
+          // if (res.statusCode != 200) {
+          //   console.log('error')
+          // } else {
+          //   navigate("event/" + res.id)
+          // }
     
         }
       }
