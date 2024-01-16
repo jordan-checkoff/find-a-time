5c4073a6842b5790ef7700ea24744e07b8433e35
Jordan Checkoff <jordan.checkoff@gmail.com>
2024-01-16 01:27:37 -0800

    Edit Calendar template

--- src/components/view_event/Calendar.tsx
+++ src/components/view_event/Calendar.tsx
@@ -1,5 +1,3 @@
-import { Drawer } from "@mui/material";
-import { useState } from "react";
 import { useEvent } from "./EventContext";
 import TimeColumn from "./TimeColumn";
 
@@ -7,38 +5,21 @@ interface props {
     title: string,
     subtitle: string,
     start: number,
-    onMouseEnter: (row: number, col: number) => () => void,
-    onMouseLeave: (row: number, col: number) => () => void,
-    onClick: (row: number, col: number) => () => void,
-    weight: (row: number, col: number) => number
+    onMouseEnter?: (row: number, col: number) => () => void,
+    onMouseDown?: (row: number, col: number) => () => void,
+    onMouseLeave?: (row: number, col: number) => () => void,
+    onMouseMove?: (row: number, col: number) => () => void,
+    onClick?: (row: number, col: number) => () => void,
+    weight: (row: number, col: number) => number,
 }
 
 
 
-export default function Calendar({title, subtitle, start, onMouseEnter, onClick, onMouseLeave, weight} : props) {
+export default function Calendar({title, subtitle, start, onMouseMove = (r, c) => () => {}, onMouseDown = (r, c) => () => {}, onMouseEnter = (r, c) => () => {}, onClick = (r, c) => () => {}, onMouseLeave = (r, c) => () => {}, weight} : props) {
 
-    const {event, calendar} = useEvent()
+    const { calendar } = useEvent()
     const numCols = calendar.get_num_cols()
     const gaps = calendar.get_breaks()
-    // const numCols = calendar.get_num_cols()
-
-    // const weight = (rowNum: number, colNum: number) => {
-    //     const size = event.availability_by_time.get(calendar.get_datetime(rowNum, colNum))?.size
-    //     if (size) {
-    //         return Math.round(size / event.availability_by_user.size * 1000) / 1000
-
-    //     } else {
-    //         return 0
-    //     }
-    // }
-
-    // const getData = (rowNum: number, colNum: number) => {
-    //     const users = event.availability_by_time.get(calendar.get_datetime(rowNum, colNum))
-    //     const date = calendar.get_dayjs(rowNum, colNum).format("M/D/YY h:mm A")
-    //     const num = users?.size
-    //     const total = event.availability_by_user.size
-    //     return {users, date, num, total}
-    // }
 
     return (
         <div>
@@ -46,7 +27,7 @@ export default function Calendar({title, subtitle, start, onMouseEnter, onClick,
             <p className="mb-8">{subtitle}</p>
             <div className="pb-10 w-full overflow-x-scroll flex select-none">
                 <TimeColumn />
-                {calendar.get_dates().slice(start, start+numCols).map((d, i) => <Column onClick={onClick} weight={weight} colNum={i} date={d} gap={gaps.has(i)} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} bottomtimes={calendar.get_bottom_blocks()} toptimes={calendar.get_top_blocks()} />)}    
+                {calendar.get_dates().slice(start, start+numCols).map((d, i) => <Column onMouseMove={onMouseMove} onMouseDown={onMouseDown} onClick={onClick} weight={weight} colNum={i + start} date={d} gap={gaps.has(i)} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} bottomtimes={calendar.get_bottom_blocks()} toptimes={calendar.get_top_blocks()} />)}    
             </div>
         </div>
 
@@ -59,25 +40,33 @@ interface ColumnProps {
     onMouseEnter: (row: number, col: number) => () => void,
     onMouseLeave: (row: number, col: number) => () => void,
     onClick: (row: number, col: number) => () => void,
+    onMouseDown: (row: number, col: number) => () => void,
+    onMouseMove: (row: number, col: number) => () => void,
     date: string,
     toptimes: string[],
     bottomtimes: string[],
     colNum: number,
-    weight: (row: number, col: number) => number
+    weight: (row: number, col: number) => number,
 }
 
-function Column({gap, date, onMouseEnter, onClick, onMouseLeave, toptimes, bottomtimes, colNum, weight}: ColumnProps) {
+function Column({gap, date, onMouseMove, onMouseEnter, onClick, onMouseDown, onMouseLeave, toptimes, bottomtimes, colNum, weight}: ColumnProps) {
+
+    const { event, calendar } = useEvent()
+
+    const exists = (row: number, col: number) => {
+        return event.availability_by_time.has(calendar.get_datetime(row, col))
+    }
 
     return (
         <div className="w-20" style={{marginRight: gap ? 10 : 0}}>
             <p className="text-center mb-2 text-xs">{date}</p>
             {toptimes.length > 0 && 
                 <div className="mb-4">
-                    {toptimes.map((t, i) => <Cell onClick={onClick(i, colNum)} weight={weight(i, colNum)} onMouseEnter={onMouseEnter(i, colNum)} onMouseLeave={onMouseLeave(i, colNum)} />)}
+                    {toptimes.map((t, i) => <Cell onMouseMove={onMouseMove(i, colNum)} onMouseDown={onMouseDown(i, colNum)} exists={exists(i, colNum)} onClick={onClick(i, colNum)} weight={weight(i, colNum)} onMouseEnter={onMouseEnter(i, colNum)} onMouseLeave={onMouseLeave(i, colNum)} />)}
                 </div>
             }
 
-            {bottomtimes.map((t, i) => <Cell onClick={onClick(i, colNum)} weight={weight(i, colNum)} onMouseEnter={onMouseEnter(i, colNum)} onMouseLeave={onMouseLeave(i, colNum)} />)}
+            {bottomtimes.map((t, i) => <Cell onMouseMove={onMouseMove(i + toptimes.length, colNum)} onMouseDown={onMouseDown(i + toptimes.length, colNum)} exists={exists(i + toptimes.length, colNum)} onClick={onClick(i + toptimes.length, colNum)} weight={weight(i + toptimes.length, colNum)} onMouseEnter={onMouseEnter(i+ toptimes.length, colNum)} onMouseLeave={onMouseLeave(i+ toptimes.length, colNum)} />)}
         </div>
     )
 }
@@ -86,81 +75,23 @@ function Column({gap, date, onMouseEnter, onClick, onMouseLeave, toptimes, botto
 interface cellProps {
     weight: number,
     onMouseEnter: () => void,
-    onMouseLeave: () => void
-    onClick: () => void
+    onMouseMove: () => void,
+    onMouseLeave: () => void,
+    onMouseDown: () => void,
+    onClick: () => void,
+    exists: boolean
 }
 
-function Cell({onMouseEnter, onMouseLeave, weight, onClick}: cellProps) {
+function Cell({onMouseEnter, onMouseMove, onMouseLeave, onMouseDown, weight, onClick, exists}: cellProps) {
+
+    if (!exists) {
+        return <div className="h-6 border-2 bg-gray-400" />
+    }
 
     return (
-        <div className={`h-6 border-2`} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
+        <div className={`h-6 border-2`} onMouseMove={onMouseMove} onMouseDown={onMouseDown} onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
                 <div style={{opacity: weight}} className={`h-full bg-red-500`}  />
         </div>
     )
 }
 
-
-// interface ColumnProps {
-//     colNum: number,
-//     toptimes: string[],
-//     bottomtimes: string[],
-//     date: string,
-//     getData: any,
-//     weight: (row: number, col: number) => number,
-//     setSelected: any,
-//     gap: boolean
-// }
-
-
-// function Column({colNum, gap, date, toptimes, bottomtimes, weight, getData, setSelected}: ColumnProps) {
-
-//     return (
-//         <div className="w-20" style={{marginRight: gap ? 10 : 0}}>
-//             <p className="text-center mb-2 text-xs">{date}</p>
-//             {toptimes.length > 0 && 
-//                 <div className="mb-4">
-//                     {toptimes.map((t, i) => <Cell setSelected={setSelected} weight={weight(i, colNum)} data={getData(i, colNum)} />)}
-//                 </div>
-//             }
-
-//             {bottomtimes.map((t, i) => <Cell setSelected={setSelected} weight={weight(i+ toptimes.length, colNum)} data={getData(i + toptimes.length, colNum)} />)}
-//         </div>
-//     )
-// }
-
-
-
-// interface cellProps {
-//     weight: number,
-//     data: any,
-//     setSelected: any
-// }
-
-// function Cell({weight, data, setSelected}: cellProps) {
-//     const [open, setOpen] = useState(false)
-
-//     if (window.innerWidth > 1000) {
-//         return (
-//             <div className={`h-6 border-2`} onMouseEnter={() => setSelected(data)} onMouseLeave={() => setSelected(null)}>
-//                 <div style={{opacity: weight}} className={`h-full bg-red-500`}  />
-//             </div>
-//         )
-//     }
-
-//     return (
-//         <>
-//             <Drawer open={open} anchor="bottom" onClose={() => setOpen(false)}>
-//                 <p>{data.date}</p>
-//                 <p>{data.num}/{data.total}</p>
-//                 {Array.from(data.users).map((u) => {
-//                     return <p>{u as string}</p>
-//                 })}
-//             </Drawer>
-//             <div className={`h-6 border-2`} onClick={() => setOpen(true)}>
-//                 <div style={{opacity: weight}} className={`h-full bg-red-500`}  />
-//             </div>
-//         </>
-//     ) 
-
-
-// }
--- src/components/view_event/EditCalendar.tsx
+++ src/components/view_event/EditCalendar.tsx
@@ -1,11 +1,11 @@
 import { useState } from "react"
-import Event, { Calendar } from "../../interfaces/Event"
+import Event, { Calendar as C } from "../../interfaces/Event"
 import { useEvent } from "./EventContext"
-import TimeColumn from "./TimeColumn"
+import Calendar from "./Calendar"
 
 interface props {
     data: Event,
-    calendar:  Calendar,
+    calendar:  C,
     startCol: number,
     endCol: number,
     user: string,
@@ -22,11 +22,6 @@ export default function EditCalendar({data, calendar, startCol, endCol, user}: p
     
     const { updateAvailability } = useEvent()
 
-    const exists = (row: number, col: number) => {
-        return data.availability_by_time.has(calendar.get_datetime(row, col))
-    }
-
-
     const isChecked = (row: number, col: number) => {
         if (row >= Math.min(start[0], end[0]) && row <= Math.max(start[0],end[0]) && col >= Math.min(start[1], end[1]) && col <= Math.max(start[1],end[1])) {
             return adding
@@ -34,16 +29,28 @@ export default function EditCalendar({data, calendar, startCol, endCol, user}: p
         return !!data.availability_by_user.get(user)?.has(calendar.get_datetime(row, col))
     }
 
+    const weight = (row: number, col: number) => {
+        if (isChecked(row, col)) {
+            return 1
+        } else {
+            return 0
+        }
+    }
+
     const handleMouseDown = (row: number, col: number) => {
-        setMouseDown(true)
-        setAdding(!data.availability_by_user.get(user)?.has(calendar.get_datetime(row, col)))
-        setStart([row, col])
-        setEnd([row, col])
+        return () => {
+            setMouseDown(true)
+            setAdding(!data.availability_by_user.get(user)?.has(calendar.get_datetime(row, col)))
+            setStart([row, col])
+            setEnd([row, col])
+        }
     }
 
     const handleMouseMove = (row: number, col: number) => {
         if (mouseDown) {
-            setEnd([row, col])
+            return () => setEnd([row, col])
+        } else {
+            return () => {}
         }
     }
 
@@ -56,66 +63,15 @@ export default function EditCalendar({data, calendar, startCol, endCol, user}: p
 
     document.onpointerup = handleMouseUp
 
-    const gaps = calendar.get_breaks()
 
     return (
-        <div>
-            <p className="mb-2 text-xl font-bold">Edit Your Availability</p>
-            <p className="mb-8">Drag to select the times when you are available.</p>
-            <div className="pb-10 w-full overflow-x-scroll flex" style={{userSelect: "none"}}>
-                <TimeColumn />
-                {calendar.get_dates().slice(startCol, endCol).map((d, i) => <Column gap={gaps.has(i)} exists={exists} handleMouseMove={handleMouseMove} handleMouseDown={handleMouseDown} checked={isChecked} date={d} toptimes={calendar.get_top_blocks()} bottomtimes={calendar.get_bottom_blocks()} colNum={i + startCol} />)}    
-            </div>
-        </div>
-
-    )
-}
-
-
-
-
-interface ColumnProps {
-    colNum: number,
-    toptimes: string[],
-    bottomtimes: string[],
-    date: string,
-    checked: (row: number, col: number) => boolean,
-    handleMouseDown: (row: number, col: number) => void,
-    handleMouseMove: (row: number, col: number) => void,
-    exists: any,
-    gap: boolean
-}
-
-
-function Column({colNum, gap, date, exists, toptimes, bottomtimes, checked, handleMouseDown, handleMouseMove}: ColumnProps) {
-
-    return (
-        <div className="w-20" style={{marginRight: gap ? 10 : 0}}>
-            <p className="text-center mb-2 text-xs">{date}</p>
-            {toptimes.length > 0 && 
-                <div className="mb-4">
-                    {toptimes.map((t, i) => <Cell exists={exists(i, colNum)} handleMouseMove={() => handleMouseMove(i, colNum)} handleMouseDown={() => handleMouseDown(i, colNum)} checked={checked(i, colNum)} />)}
-                </div>
-            }
-            {bottomtimes.map((t, i) => <Cell exists={exists(i + toptimes.length, colNum)} handleMouseMove={() => handleMouseMove(i + toptimes.length, colNum)} handleMouseDown={() => handleMouseDown(i + toptimes.length, colNum)} checked={checked(i + toptimes.length, colNum)} />)}
-        </div>
-    )
-}
-
-interface cellProps {
-    checked: boolean
-    handleMouseDown: () => void
-    handleMouseMove: () => void
-    exists: boolean
-}
-
-function Cell({exists, checked, handleMouseDown, handleMouseMove}: cellProps) {
-
-    if (!exists) {
-        return <div className="h-6 border-2 bg-gray-400" />
-    }
-
-    return (
-        <div className="h-6 border-2" onPointerDown={handleMouseDown} onPointerMove={handleMouseMove} style={{backgroundColor: checked ? "red" : "white"}} />
+        <Calendar
+            title="Edit Your Availability"
+            subtitle="Drag to select the times when you are available."
+            start={startCol}
+            weight={weight}
+            onMouseDown={handleMouseDown}
+            onMouseMove={handleMouseMove}
+        />
     )
 }
