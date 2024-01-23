21c54355327bd3bc63a2131c5affa63df4acb262
Jordan Checkoff <jordan.checkoff@gmail.com>
2024-01-22 21:25:52 -0800

    nav button labels

--- src/components/view_event/NavButtons.tsx
+++ src/components/view_event/NavButtons.tsx
@@ -15,8 +15,8 @@ export default function NavButtons({start, setStart}: props) {
     return (
         <div className="w-full">
             <div className="grid grid-cols-2 gap-2 mb-4">
-                <Button onClick={() => setStart(start - 1)} text="<" disabled={start == 0} />
-                <Button onClick={() => setStart(start + 1)} text=">" disabled={start + calendar.get_num_cols() >= calendar.dates.length} />
+                <Button onClick={() => setStart(start - 1)} text="&larr; Previous" disabled={start == 0} />
+                <Button onClick={() => setStart(start + 1)} text="Next &rarr;" disabled={start + calendar.get_num_cols() >= calendar.dates.length} />
             </div>
         </div>
     )
